702b5ff8393ea11251848ad49440518e5241b9d0
Pebry Andrian <febryandrianjr@gmail.com>
2024-10-20 16:17:40 +0700

    Controller

---
+++ MVC/app/controllers/About.php
@@ -0,0 +1,13 @@
+<?php
+
+class About {
+    public function index($nama = 'Pebry Andrian', $pekerjaan = 'Mahasiswa') 
+    {
+        echo "Hallo, nama saya $nama saya adalah $pekerjaan";
+    }
+    public function page() 
+    {
+        echo 'About/page';
+    }
+
+}
