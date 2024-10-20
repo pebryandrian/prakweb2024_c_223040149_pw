7d2953437f1ac73a4e684892955ca9546c818099
Pebry Andrian <febryandrianjr@gmail.com>
2024-10-20 13:47:28 +0700

    Constant

---
+++ oophp/constant.php
@@ -0,0 +1,43 @@
+<?php
+
+//define ('NAMA', 'Pebry Andrian');
+//echo NAMA;
+
+//echo "<br>";
+
+//const UMUR =  20;
+//echo UMUR;
+
+// class Coba {
+//     const NAMA = 'Pebry Andrian';
+// }
+
+// echo  Coba::NAMA;
+
+
+
+// echo __FILE__;
+
+
+// function coba(){
+//     return __FUNCTION__;
+//     }
+
+//     echo coba();
+
+class Coba {
+    public $kelas = __CLASS__;
+}
+
+$obj = new Coba();
+echo $obj->kelas;
+
+
+
+
+
+
+
+
+
+?>
